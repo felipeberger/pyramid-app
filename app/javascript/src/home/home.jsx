44298ec32ec2a5ef4ba3398d44717e5da805433b
felipeberger <feconchaber@gmail.com>
2021-12-03 16:20:01 +1100

    rebuilt rails react app codebase due to remove turbolinks and other unnecessary features

---
+++ app/javascript/src/home/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => (
+    <>
+    <h1>Home Page</h1>
+    <h2>Chaging something else</h2>
+    <h3>Another one</h3>
+    </>
+)
+
+export default Home
+
