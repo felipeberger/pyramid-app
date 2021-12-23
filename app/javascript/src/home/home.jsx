79e31ae9c7f8287ab86b035fccde5a922f78d0c9
felipeberger <feconchaber@gmail.com>
2021-12-23 12:03:04 +1100

    updated home and home/index to use the redux store for global state. Home now uses global state for keeping track of user ID. Also updated the index file to import and display the layout instead of having the layout component implemented in the home component for ease of readability and maintainability

--- app/javascript/src/home/home.jsx
+++ app/javascript/src/home/home.jsx
@@ -3,30 +3,34 @@ import Layout from '../layout/layout';
 import Grid from '@mui/material/Grid';
 import StoryCard from '../storyCard/storyCard';
 import AddNewStory from './addNewStory';
-import {handleErrors} from '../utils/fetchHelper'
+import {handleErrors} from '../utils/fetchHelper';
+import { useSelector } from 'react-redux';
+
 
 export default function Home () {
     const [stories, setStories] = useState(null);
     const [loaded, setLoaded] = useState(false);
-    const [userId, setUserId] = useState(1)
 
-    const cardWidth = '450px';
-    const cardHeight = '400px';
-
-    // TODO Add redux for state management so logged-in status and current stories are kept standardized accross all views
+    // redux selector functions
+    const retrieveUserId = state => state.userId
     
-    // TODO fetch/get logged in user ID number to pass on to stories fetch request
+    // redux subscriptions (global state)
+    const globalUserId = useSelector(retrieveUserId).userId
 
     // TODO useEffect triggers twice on load of page, fix so only loads once 
-
     useEffect( ()=> {
-        fetch(`/api/stories/${userId}/getall`)
+        fetch(`/api/stories/${globalUserId}/getall`)
             .then(handleErrors)
             .then(data => {
                 setStories(data.user.stories)
                 setLoaded(true)
             })
     }, [loaded])
+
+    // constants used for UI dimensions
+    const cardWidth = '450px';
+    const cardHeight = '400px';
+    
 
     const createStoryCards = () => {
         if (!loaded) return
@@ -46,7 +50,7 @@ export default function Home () {
                     >
                         {/* checking array has own property answer obtained from https://stackoverflow.com/questions/13107855/how-to-check-if-an-array-index-exists-or-not-in-javascript
 
-                        Did not use the most upvoted answer but the one that suggests using object inheritance as an array in JS is jus an object under the hood */}
+                        Did not use the most upvoted answer but the one that suggests using object inheritance as an array in JS is just an object under the hood */}
                          <StoryCard 
                             width={cardWidth}
                             height={cardHeight}
@@ -62,29 +66,27 @@ export default function Home () {
     }
         
     return (
-        <Layout>
-            <Grid
-                item
-                container 
-                spacing={3} 
-                direction='row'
-                alignContent='center' 
-                justify='center'
-                sx={{minHeight:'88vh'}}
-                my={{sm:2, md:0}}
-                >
+        <Grid
+            item
+            container 
+            spacing={3} 
+            direction='row'
+            alignContent='center' 
+            justify='center'
+            sx={{minHeight:'88vh'}}
+            my={{sm:2, md:0}}
+            >
 
-                {loaded? createStoryCards() : ""}
+            {loaded? createStoryCards() : ""}
 
-                {loaded? createStoryCards() : ""}
+            {loaded? createStoryCards() : ""}
 
-                {loaded? createStoryCards() : ""}
+            {loaded? createStoryCards() : ""}
 
-                <Grid item container xs={12} md={6} xl={4} align='center' alignContent='center' justifyContent='center' px={1} py={2} sx={{minHeight: "300px"}} >
-                    <AddNewStory width={cardWidth}/>
-                </Grid>
+            <Grid item container xs={12} md={6} xl={4} align='center' alignContent='center' justifyContent='center' px={1} py={2} sx={{minHeight: "300px"}} >
+                <AddNewStory width={cardWidth}/>
             </Grid>
-        </Layout>
+        </Grid>
     )
 }
 
--- app/javascript/src/home/index.jsx
+++ app/javascript/src/home/index.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Home from './home';
+import Layout from '../layout/layout';
+import { Provider } from 'react-redux';
+import { store } from '../../global_state/store';
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Home />,
+    <Provider store={store}>
+      <Layout>
+        <Home />
+      </Layout>
+    </Provider>,
     document.body.appendChild(document.createElement('div')),
   )
 })
