f78cdb5c087507129e2f18d6ed33a12c06252b6b
felipeberger <feconchaber@gmail.com>
2022-02-01 10:33:11 +1100

    updated model so argument is not necessary to create an argument record. Purpose is to allow the creation of empty records that are used to initialize new stories

--- app/models/argument.rb
+++ app/models/argument.rb
@@ -2,7 +2,7 @@ class Argument < ApplicationRecord
 
     belongs_to :insight
 
-    validates :argument, presence: true
+    validates :argument, presence: false
     validates :explanation, presence: false
     validates :insight_id, presence: true
 
