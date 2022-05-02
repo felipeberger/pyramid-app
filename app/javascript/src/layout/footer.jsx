e40c1d7d84188f0babf50d6c7f134e7d0b439994
felipeberger <feconchaber@gmail.com>
2022-05-02 23:45:42 +1100

    removed unused footer.css import

--- app/javascript/src/layout/footer.jsx
+++ app/javascript/src/layout/footer.jsx
@@ -5,7 +5,6 @@ import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import EmailIcon from '@mui/icons-material/Email';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import './footer.css'
 
 const Footer = () => {
 
