554d1a96b3420e1cb4d3a93dbdae3e3f15a9bf20
felipeberger <feconchaber@gmail.com>
2022-05-02 22:40:51 +1100

    updater footer positioning

--- app/javascript/src/layout/footer.jsx
+++ app/javascript/src/layout/footer.jsx
@@ -5,12 +5,13 @@ import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import EmailIcon from '@mui/icons-material/Email';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import './footer.css'
 
 const Footer = () => {
 
     return (
 
-        <Box sx={{width: '100%' }} disableGutters >
+        <Box sx={{width: '100%' }} id="footer" disableGutters >
             <BottomNavigation showLabels >
                 <BottomNavigationAction
                     label='Github' 
